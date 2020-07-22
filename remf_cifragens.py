5e89e81523c4b4fa2563e2404bbaf57dd029f413
Roan Franklin <roanfranklin@gmail.com>
2020-07-22 15:27:50 -0300

    Adicionado o script em python3 com as cifras (Cesar, Text2Hex, Hex2Text, MiletPolar, TenisPolar e ZenitPolar).

---
+++ remf_cifragens.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python3
+# pip install pyfiglet
+
+import os, sys, binascii, re
+#import pyfiglet
+
+def ccesar(chave, mensagem):
+    # número das letras nas extremidades do alfabeto
+    nA = ord('A')
+    nZ = ord('Z')
+    na = ord('a')
+    nz = ord('z')
+
+    cifrada = ""
+    for caracter in mensagem:
+        # achar no alfabeto a letra que esteja chave posições a frente
+        ind = ord(caracter)
+        # Se estiver no intervalo de letras maiúsculas
+        if nA <= ind <= nZ:
+            nova_letra = chr((ind + chave)%(nZ+1) + ((ind + chave)//(nZ+1))*nA)
+            # substituir na mensagem a letra pela nova_letra
+            cifrada = cifrada + nova_letra
+        # Outra forma de verificar se esta no intervalo de letras (agora) minúsculas
+        # lembrar que range gera intervalos da forma [a, b), portanto somamos 1
+        elif ind in range(na, nz + 1):
+            nova_letra = chr((ind + chave)%(nz+1) + ((ind + chave)//(nz+1))*na)
+            cifrada = cifrada + nova_letra
+        # Se não for letra
+        else:
+            cifrada = cifrada + caracter
+
+    return cifrada
+
+def txt2hex(mensagem):
+    # Converte um texto para Hex
+    encoded = binascii.hexlify(bytes(mensagem, "utf-8"))
+    encoded = str(encoded).strip("b")
+    encoded = encoded.strip("'")
+    return encoded
+
+def hex2txt(mensagem):
+    # Converte Hex para texto(ASCII)
+    decoded = binascii.unhexlify(bytes(mensagem, "utf-8"))
+    decoded = str(decoded).strip("b")
+    decoded = decoded.strip("'")
+    return decoded
+
+def miletpolar(mensagem):
+    replaces = {'m':'p',
+                'i':'o',
+                'l':'l',
+                'e':'a',
+                't':'r',
+                'M':'P',
+                'I':'O',
+                'L':'L',
+                'E':'A',
+                'T':'R',
+                'p':'m',
+                'o':'i',
+                'l':'l',
+                'a':'e',
+                'r':'t',
+                'P':'M',
+                'O':'I',
+                'L':'L',
+                'A':'E',
+                'R':'T'
+                }
+    regex = re.sub("|".join(replaces.keys()), lambda match: replaces[match.string[match.start():match.end()]], mensagem)
+    return regex
+
+def tenispolar(mensagem):
+    replaces = {'t':'p',
+                'e':'o',
+                'n':'l', 
+                'i':'a', 
+                's':'r',
+                'T':'P',
+                'E':'O',
+                'N':'L',
+                'I':'A',
+                'S':'R',
+                'p':'t',
+                'o':'e',
+                'l':'n',
+                'a':'i',
+                'r':'s',
+                'P':'T',
+                'O':'E',
+                'L':'N',
+                'A':'I',
+                'R':'S'
+                }
+    regex = re.sub("|".join(replaces.keys()), lambda match: replaces[match.string[match.start():match.end()]], mensagem)
+    return regex
+
+def zenitpolar(mensagem):
+    replaces = {'z':'p',
+                'e':'o',
+                'n':'l',
+                'i':'a',
+                't':'r',
+                'Z':'P',
+                'E':'O',
+                'N':'L',
+                'I':'A',
+                'T':'R',
+                'p':'z',
+                'o':'e',
+                'l':'n',
+                'a':'i',
+                'r':'t',
+                'P':'Z',
+                'O':'E',
+                'L':'N',
+                'A':'I',
+                'R':'T'
+                }
+    regex = re.sub("|".join(replaces.keys()), lambda match: replaces[match.string[match.start():match.end()]], mensagem)
+    return regex
+
+def ajuda():
+    os.system("clear")
+#    ascii_banner = pyfiglet.figlet_format("REMF.COM.BR - Cifragens")
+#    print(ascii_banner)
+    print(' REMF.COM.BR')
+    print(' ')
+    print(' Use: ', sys.argv[0], '-cc [ ROT = Numero 1 à 26 ] "Frase a ser cifrada com cifra de cesar usando um ROT específico."')
+    print('      ', sys.argv[0], '-cc "Frase a ser cifrada com cifra de cesar usando todos ROT 1 à 26."')
+    print('      ', sys.argv[0], '-t2h "Frase a ser coverditida de texto em hexadecinal."')
+    print('      ', sys.argv[0], '-h2t "Frase a ser coverditida de hexadecinal em texto."')
+    print('      ', sys.argv[0], '-mp "Cifrar o texto usando substituição MILET POLAR."')
+    print('      ', sys.argv[0], '-tp "Cifrar o texto usando substituição TENIS POLAR."')
+    print('      ', sys.argv[0], '-zp "Cifrar o texto usando substituição ZENIT POLAR."')
+    print(' ')
+    quit()
+
+def main():
+#    print("     Nome do script: ", sys.argv[0])
+#    print("Total de argumentos: ", len(sys.argv))
+#    print("Lista de argumentos: ", str(sys.argv))
+#    quit()
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-cc':
+            if len(sys.argv) == 3:
+                mensagem = str(sys.argv[2])
+                contador = 1
+                while ( contador < 27 ):
+                    cifrada = ccesar(contador, mensagem)
+                    print(contador,' ', cifrada)
+                    contador = contador + 1
+                quit()
+            elif len(sys.argv) == 4:
+                chave = int(sys.argv[2])
+                mensagem = str(sys.argv[3])
+                cifrada = ccesar(chave, mensagem)
+            else:
+                ajuda()
+        elif sys.argv[1] == '-t2h':
+            if len(sys.argv) == 3:
+                mensagem = str(sys.argv[2])
+                cifrada = txt2hex(mensagem)
+            else:
+                ajuda()
+        elif sys.argv[1] == '-h2t':
+            if len(sys.argv) == 3:
+                mensagem = str(sys.argv[2])
+                cifrada = hex2txt(mensagem)
+            else:
+                ajuda()
+        elif sys.argv[1] == '-mp':
+            if len(sys.argv) == 3:
+                mensagem = str(sys.argv[2])
+                cifrada = miletpolar(mensagem);
+            else:
+                ajuda()
+        elif sys.argv[1] == '-tp':
+            if len(sys.argv) == 3:
+                mensagem = str(sys.argv[2])
+                cifrada = tenispolar(mensagem);
+            else:
+                ajuda()
+        elif sys.argv[1] == '-zp':
+            if len(sys.argv) == 3:
+                mensagem = str(sys.argv[2])
+                cifrada = zenitpolar(mensagem);
+            else:
+                ajuda()
+        else:
+            ajuda()
+    else:
+        ajuda()
+
+    print(cifrada)
+
+if __name__ == "__main__":
+    main()
+
